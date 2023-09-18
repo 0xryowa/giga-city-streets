b4ad9792e956624a34979a5c624b5e2c88dd7db1
0xryowa <ryowa@boom.haus>
2023-09-18 20:37:26 +0200

    Adding some basic functionality

--- src/GigaCityStreets.sol
+++ src/GigaCityStreets.sol
@@ -4,44 +4,105 @@ pragma solidity ^0.8.18;
 import "erc721a/ERC721A.sol";
 import "operator-filter-registry/UpdatableOperatorFilterer.sol";
 import "openzeppelin-contracts/access/Ownable.sol";
-import "solmate/utils/ReentrancyGuard.sol";
+import "openzeppelin-contracts/security/ReentrancyGuard.sol";
 
-// import "solmate/utils/MerkleProofLib.sol";
+// =============================================================
+//
+//   ▄████  ██▓  ▄████  ▄▄▄       ▄████▄   ██▓▄▄▄█████▓▓██   ██▓
+//  ██▒ ▀█▒▓██▒ ██▒ ▀█▒▒████▄    ▒██▀ ▀█  ▓██▒▓  ██▒ ▓▒ ▒██  ██▒
+// ▒██░▄▄▄░▒██▒▒██░▄▄▄░▒██  ▀█▄  ▒▓█    ▄ ▒██▒▒ ▓██░ ▒░  ▒██ ██░
+// ░▓█  ██▓░██░░▓█  ██▓░██▄▄▄▄██ ▒▓▓▄ ▄██▒░██░░ ▓██▓ ░   ░ ▐██▓░
+// ░▒▓███▀▒░██░░▒▓███▀▒ ▓█   ▓██▒▒ ▓███▀ ░░██░  ▒██▒ ░   ░ ██▒▓░
+//  ░▒   ▒ ░▓   ░▒   ▒  ▒▒   ▓▒█░░ ░▒ ▒  ░░▓    ▒ ░░      ██▒▒▒ 
+//   ░   ░  ▒ ░  ░   ░   ▒   ▒▒ ░  ░  ▒    ▒ ░    ░     ▓██ ░▒░ 
+// ░ ░   ░  ▒ ░░ ░   ░   ░   ▒   ░         ▒ ░  ░       ▒ ▒ ░░  
+//       ░  ░        ░       ░  ░░ ░       ░            ░ ░     
+//                              ░                      ░ ░     
+//
+// Venture into Giga City's towering shadows, where resilience
+// is the only currency of survival.
+
+// =============================================================
+//                             ERRORS
+// =============================================================
 
 error TokenDoesNotExist();
+error TransferFailed();
+error TotalSupplyExceeded();
+error PublicMintOff();
+error TxQuantityExceeded();
+error AddressQuantityExceeded();
 
-contract GigaCityStreets is UpdatableOperatorFilterer(
-        0x0000000000000000000000000000000000000000,
-        0x0000000000000000000000000000000000000000,
-        false
-    ), ERC721A, Ownable {
+// =============================================================
+//                       GIGA CITY STREETS
+// =============================================================
 
-    string public uriPrefix = "https://server.com/";
-    string public uriSuffix = ".json";
+contract GigaCityStreets is ERC721A, UpdatableOperatorFilterer, ReentrancyGuard, Ownable {
 
-    // Corpo merkle root
-    // bytes32 private _whitelistRoot;
+    // Not mintable for public, most likely. I might open it up eventually,
+    // if there will be damnd. Keeping it closed for now.
+    bool public mintOpen;
+    
+    // Lets initialize the value for 0, paying / tipping will be optional
+    uint256 public mintPrice;
 
-    uint256 public cost;
-    uint256 public maxSupply;
-    uint256 public maxMintAmountPerTx;
+    // Limit will be 1337, but added a setter that can change
+    // this value at any time.
+    uint256 public supplyCap;
+
+    // Where is the metadata hosted
+    string private _uriPrefix;
+
+    // For now, we will initialize it with .json but might not be needed
+    // when/if we transition on a server
+    string private _uriSuffix;
+
+    // Minting is limited to X per Tx. 
+    uint256 private _maxMintPerTx;
+
+    // Minting is limited to X per addy.
+    uint256 private _maxMintPerAddress;
+
+    // Mapping whitelist allocation
+    mapping(address => uint256) private _mintAllocation;
 
     // =============================================================
     //                           CONSTRUCTOR
     // =============================================================
 
-    constructor() ERC721A("Giga City Streets", "GCS") {
-        cost = 0;
-        maxSupply = 1337;
-        maxMintAmountPerTx = 1;
+    constructor(address operatorFilterRegistry_)
+        ERC721A("Giga City Streets", "GCS")
+        UpdatableOperatorFilterer(operatorFilterRegistry_,0x0000000000000000000000000000000000000000,false) {
+        mintOpen = false;
+        mintPrice = 0;
+        supplyCap = 1337;
+
+        _maxMintPerTx = 1;
+        _maxMintPerAddress = 1;
+
+        _uriSuffix = '.json';
     }
 
     // =============================================================
     //                             MINT
     // =============================================================
 
-    function mint(uint256 quantity) external payable {
-        // (bytes32[] calldata proof_, uint256 quantity_)
+
+    modifier mintable(uint256 quantity_) {
+        // Are we exceeding the supply cap? If yeah revert.
+        if (totalSupply() + quantity_ > supplyCap) revert TotalSupplyExceeded();
+        // Are we minting above transaction limit? If yeah revert.
+        if (quantity_ < 0 || quantity_ > _maxMintPerTx) revert TxQuantityExceeded();
+        // Are we minting more than what is the maximum mint per address? If yeah revert.
+        if (_numberMinted(msg.sender) == _maxMintPerAddress || _numberMinted(msg.sender) + quantity_ > _maxMintPerAddress ) revert AddressQuantityExceeded();
+        // We proceed ...
+        _;
+    }
+
+
+    function mintPublic(uint256 quantity_) external payable mintable(quantity_) {
+        // Are we exceeding the supply cap? If yeah revert.
+        if (!mintOpen) revert PublicMintOff();
 
         // require(!paused, 'The minting is paused!');
         // require(quantity > 0 && quantity <= maxMintAmountPerTx, 'Invalid mint amount!');
@@ -51,44 +112,53 @@ contract GigaCityStreets is UpdatableOperatorFilterer(
         // bytes32 leaf = keccak256(abi.encodePacked(_msgSenderERC721A()));
         // if (!MerkleProofLib.verify(proof_, _corpoRoot, leaf)) revert CantMintCorpo();
 
-        _safeMint(msg.sender, quantity);
+        _safeMint(msg.sender, quantity_);
+    }
+
+    // =============================================================
+    //                            WHITELIST
+    // =============================================================
+
+    function setWhitelist(address wlAddress_, uint256 wlAllocation_) external{
+        _mintAllocation[wlAddress_] = wlAllocation_;
     }
 
     // =============================================================
     //                           MANAGEMENT
     // =============================================================
 
-    function setUriPrefix(uint256 uriPrefix_) external{
-        uriPrefix = uriPrefix_;
+    function setMintOpen(bool mintOpen_) external {
+        mintOpen = mintOpen_;
     }
 
-    function setUriSuffix(uint256 uriSuffix_) external{
-        uriSuffix = uriSuffix_;
+    function setUriPrefix(string calldata uriPrefix_) external{
+        _uriPrefix = uriPrefix_;
     }
 
-    function setCost(uint256 cost_) external{
-        cost = cost_;
+    function setUriSuffix(string calldata uriSuffix_) external{
+        _uriSuffix = uriSuffix_;
     }
 
-    function setMaxSupply(uint256 maxSupply_) external{
-        maxSupply = maxSupply_;
+    function setMintPrice(uint256 mintPrice_) external{
+        mintPrice = mintPrice_;
     }
 
-    function setMaxMintAmountPerTx(uint256 maxMintAmountPerTx_) external{
-        maxMintAmountPerTx = maxMintAmountPerTx_;
+    function setSupplyCap(uint256 supplyCap_) external{
+        supplyCap = supplyCap_;
     }
 
-    function setCorpoRoot(bytes32 newRoot_) external onlyOwner {
-        _whitelistRoot = newRoot_;
+    function setMaxMintPerTx(uint256 maxMintPerTx_) external{
+        _maxMintPerTx = maxMintPerTx_;
+    }
+
+    function setMaxMintPerAddress(uint256 maxMintPerAddress_) external{
+        _maxMintPerAddress = maxMintPerAddress_;
     }
 
     // =============================================================
     //                           OWNABLE
     // =============================================================
 
-    /**
-     * @dev Returns the address of the current owner.
-     */
     function owner() public view virtual override(UpdatableOperatorFilterer, Ownable) returns (address) {
         return super.owner();
     }
@@ -98,7 +168,7 @@ contract GigaCityStreets is UpdatableOperatorFilterer(
     // =============================================================
 
     function _baseURI() internal view virtual override returns (string memory) {
-        return uriPrefix;
+        return _uriPrefix;
     }
 
     function tokenURI(uint256 _tokenId) public view virtual override(ERC721A) returns (string memory) {
@@ -106,7 +176,7 @@ contract GigaCityStreets is UpdatableOperatorFilterer(
 
         string memory currentBaseURI = _baseURI();
         return bytes(currentBaseURI).length > 0
-            ? string(abi.encodePacked(currentBaseURI, _toString(_tokenId), uriSuffix))
+            ? string(abi.encodePacked(currentBaseURI, _toString(_tokenId), _uriSuffix))
             : '';
     }
 
--- test/GigaCityStreets.t.sol
+++ test/GigaCityStreets.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.18;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {GigaCityStreets} from "../src/GigaCityStreets.sol";
+import "operator-filter-registry/OperatorFilterRegistry.sol";
 
 contract GigaCityStreetsTest is Test {
     address public owner;
@@ -10,28 +11,98 @@ contract GigaCityStreetsTest is Test {
     address public jerry = makeAddr("jerry");
 
     GigaCityStreets public gigaStreets;
+    OperatorFilterRegistry public registry;
 
     function setUp() public {
         // new deployed contracts will have Test as owner
         owner = address(this); 
 
-        gigaStreets = new GigaCityStreets();
+        registry = new OperatorFilterRegistry();
+        address registryAddress = address(registry);
+
+        gigaStreets = new GigaCityStreets(registryAddress);
+
+        gigaStreets.setMintPrice(0);
+        gigaStreets.setSupplyCap(5);
+        gigaStreets.setMaxMintPerTx(5);
+        gigaStreets.setMaxMintPerAddress(5);
     }
 
-    function test_Increment() public {
-        bytes4 selector = bytes4(keccak256("TokenDoesNotExist()"));
-        vm.expectRevert(abi.encodeWithSelector(selector));
-        gigaStreets.tokenURI(0);
-        
+    function test_initValues() public {
+        assertEq(gigaStreets.mintOpen(), false);
+        assertEq(gigaStreets.mintPrice(), 0);
+        assertEq(gigaStreets.supplyCap(), 5);
+    }
+
+    function test_mintable_totalSupply() public {
+        gigaStreets.setMintOpen(true);
+
         vm.prank(tom);
-        gigaStreets.mint(1); 
+        gigaStreets.mintPublic(2);
+
+        bytes4 err1 = bytes4(keccak256("TotalSupplyExceeded()"));
+        vm.expectRevert(abi.encodeWithSelector(err1));
 
         vm.prank(jerry);
-        gigaStreets.mint(1); 
+        gigaStreets.mintPublic(4);
 
-        gigaStreets.tokenURI(0);
-
-        assertEq(gigaStreets.ownerOf(0), tom);
-        assertEq(gigaStreets.ownerOf(1), jerry);
     }
+
+    function test_mintable_maxMintPerTx() public {
+        gigaStreets.setMintOpen(true);
+        gigaStreets.setMaxMintPerTx(3);
+
+        bytes4 err2 = bytes4(keccak256("TxQuantityExceeded()"));
+        vm.expectRevert(abi.encodeWithSelector(err2));
+
+        vm.prank(tom);
+        gigaStreets.mintPublic(4);
+
+    }
+
+    function test_mintable_maxMintPerAddress() public {
+        gigaStreets.setMintOpen(true);
+        gigaStreets.setMaxMintPerAddress(3);
+        gigaStreets.setMaxMintPerTx(5);
+
+
+        vm.prank(tom);
+        gigaStreets.mintPublic(2);
+
+        bytes4 err3 = bytes4(keccak256("AddressQuantityExceeded()"));
+        vm.expectRevert(abi.encodeWithSelector(err3));
+
+        vm.prank(tom);
+        gigaStreets.mintPublic(2);
+    }
+
+    function test_mintPublic() public {
+        bytes4 selector = bytes4(keccak256("PublicMintOff()"));
+        vm.expectRevert(abi.encodeWithSelector(selector));
+        
+        vm.prank(tom);
+        gigaStreets.mintPublic(1);
+
+        gigaStreets.setMintOpen(true);
+
+        vm.prank(tom);
+        gigaStreets.mintPublic(1);
+    }
+
+    // function test_Increment() public {
+    //     bytes4 selector = bytes4(keccak256("TokenDoesNotExist()"));
+    //     vm.expectRevert(abi.encodeWithSelector(selector));
+    //     gigaStreets.tokenURI(0);
+        
+    //     vm.prank(tom);
+    //     gigaStreets.mint(1); 
+
+    //     vm.prank(jerry);
+    //     gigaStreets.mint(1); 
+
+    //     gigaStreets.tokenURI(0);
+
+    //     assertEq(gigaStreets.ownerOf(0), tom);
+    //     assertEq(gigaStreets.ownerOf(1), jerry);
+    // }
 }
